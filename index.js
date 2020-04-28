68a6a6e140c6e5ac014712194a482473aec9a98f
Denish Puthawala <puthawala.denish@gmail.com>
2020-04-28 19:45:42 +0530

    project setup

---
+++ index.js
@@ -0,0 +1,22 @@
+// init code
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
+const port = process.env.PORT;
+
+// middleware
+app.use(morgan('dev'));
+app.use(cors());
+
+// routes
+app.all('/', (req, res) => {
+    res.json({
+        status: true,
+        message: "index page route is running"
+    });
+});
+
+// server start
+app.listen(port, () => console.log('Server is running'));
